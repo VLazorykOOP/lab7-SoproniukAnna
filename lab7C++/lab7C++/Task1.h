2f7c321dae027f1246bf203462abb87e8bdbab8a
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2023-05-08 18:55:51 +0300

    Task1

--- lab7C++/lab7C++/Task1.h
+++ lab7C++/lab7C++/Task1.h
@@ -13,4 +13,14 @@ T minElement(T* arr, int size)
     return minElem;
 }
 
+template<>
+char minElement(char* arr, int size)
+{
+    char minElem = arr[0];
+    for (int i = 1; i < size; i++)
+        if (arr[i] < minElem)
+            minElem = arr[i];
+
+    return minElem;
+}
 
--- lab7C++/lab7C++/lab7C++.cpp
+++ lab7C++/lab7C++/lab7C++.cpp
@@ -5,20 +5,107 @@
 #include "Task1.h"
 using namespace std;
 
+void Task1Char()
+{
+    int size = 0;
+    cout << "Input size: ";
+    cin >> size;
+    char* arr{ new char[size] };
+
+    cout << "Input array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Input [ " << i + 1 << " ] element: ";
+        cin >> arr[i];
+    }
+
+    char minElem = minElement(arr, size);
+    cout << "Minimum element: " << minElem << endl;
+}
+void Task1AllType()
+{
+    int size = 0;
+    cout << "Input size: ";
+    cin >> size;
+    double* arr{ new double[size] };
+
+    cout << "Input array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Input [ " << i + 1 << " ] element: ";
+        cin >> arr[i];
+    }
+
+    double minElem = minElement(arr, size);
+    cout << "Minimum element: " << minElem << endl;
+}
+void MainMenuTask1()
+{
+    int select = 0;
+    do
+    {
+        cout << '\t' << '\t' << '\t' << "Main menu" << endl;
+        cout << "1. Another type " << endl;
+        cout << "2. Char " << endl;
+        cout << "0. Exit" << endl;
+        cin >> select;
+        system("cls");
+
+        switch (select)
+        {
+        case 1:
+            system("cls");
+            Task1AllType();
+            break;
+        case 2:
+            system("cls");
+            Task1Char();
+            break;
+        default:
+            system("cls");
+            break;
+        }
+    } while (select != 0);
+}
+
+
+void MainMenu()
+{
+    int select = 0;
+    do
+    {
+        cout << '\t' << '\t' << '\t' << "Main menu" << endl;
+        cout << "1. Task 1" << endl;
+        cout << "2. Task 2" << endl;
+        cout << "3. Task 3" << endl;
+        cout << "4. Task 4" << endl;
+        cout << "0. Exit" << endl;
+        cin >> select;
+        system("cls");
+
+        switch (select)
+        {
+        case 1:
+            system("cls");
+            MainMenuTask1();
+            break;
+        case 2:
+            system("cls");
+            break;
+        case 3:
+            system("cls");
+            break;
+        case 4:
+            system("cls");
+            break;
+        default:
+            system("cls");
+            break;
+        }
+    } while (select != 0);
+}
+
 int main()
 {
-	int size = 0;
-	cout << "Input size: ";
-	cin >> size;
-	double* arr{ new double[size] };
-
-	cout << "Input array:" << endl;
-	for (int i = 0; i < size; i++)
-	{
-		cout << "Input [ " << i + 1 << " ] element: ";
-		cin >> arr[i];
-	}
-
-	int minElem = minElement(arr, size);
-	cout << "Minimum element: " << minElem << endl;
+    MainMenu();
 }
